9e1285bbff007e7a9fa5e89c802c560ae842a558
Maxim Khrypunov <khrypunovmax@gmail>
2024-03-01 19:23:26 +0200

    SubskillMicroSkillServiceTest

---
+++ src/test/java/com/subskill/service/SubskillMicroSkillServiceTest.java
@@ -0,0 +1,14 @@
+package com.subskill.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SubskillMicroSkillServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
