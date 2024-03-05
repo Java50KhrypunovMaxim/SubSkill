90bcfafbf4fbeec33418c4bbe73be7a075bb243b
kasjke <kasmatuy2k17@gmail.com>
2024-03-05 16:00:48 +0200

    small s

--- src/test/java/com/subskill/service/SubSkillMicroSkillServiceTest.java
+++ src/test/java/com/subskill/service/SubSkillMicroSkillServiceTest.java
@@ -13,13 +13,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
