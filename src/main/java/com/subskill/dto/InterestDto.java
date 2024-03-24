29e32aef846d074b480fc3f040e86b44a1ea7e6b
kasjke <kasmatuy2k17@gmail.com>
2024-03-24 14:14:21 +0200

    upd

--- src/main/java/com/subskill/dto/InterestDto.java
+++ src/main/java/com/subskill/dto/InterestDto.java
@@ -9,7 +9,7 @@ import static com.subskill.api.ValidationConstants.*;
 
 public record InterestDto(@NotEmpty(message = INTEREST_ID_MISSING)
                           Long id,
-                          @NotEmpty(message = USER_NOT_FOUND)
+//                          @NotEmpty(message = USER_NOT_FOUND)
                           List<UserDto> user,
                           @NotEmpty(message = INTEREST_NAME_NOT_FOUND)
                           String name){
